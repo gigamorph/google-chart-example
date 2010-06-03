722fe74a82000bc0eeaaa880fd6fde1fb213da41
Seong-June Kim <jkim707@gmail.com>
2010-06-03 07:57:34 -0400

    Put declarations in an anonymous function call.
    Not that it is necessary but to make it look consistent with other files

--- src/defs.js
+++ src/defs.js
@@ -1,36 +1,45 @@
-if (typeof net == 'undefined' || !net) {
-    var net = {};
-}
-if (!net.yy) {
-    net.yy = {};
-}
-if (!net.yy.google) {
-    net.yy.google = {};
-}
-if (!net.yy.google.chart) {
-    net.yy.google.chart = {};
-}
-if (!net.yy.google.chart.param) {
-    net.yy.google.chart.param = {};
-}
-if (!net.yy.google.chart.type) {
-    net.yy.google.chart.type = {};
-}
-if (!net.yy.google.chart.util) {
-    net.yy.google.chart.util = {};
-}
+/* FILE defs.js */
 
-net.yy.google.chart.offline_test_mode = false;
+/* Seong-June Kim 2010 */
 
-if (!console) {
-    var console = { log: function() {} };
-}
+(function () {
 
-var debug = console.log;
+    if (typeof net == 'undefined' || !net) {
+        this.net = {};
+    }
+    if (!net.yy) {
+        net.yy = {};
+    }
+    if (!net.yy.google) {
+        net.yy.google = {};
+    }
+    if (!net.yy.google.chart) {
+        net.yy.google.chart = {};
+    }
+    if (!net.yy.google.chart.param) {
+        net.yy.google.chart.param = {};
+    }
+    if (!net.yy.google.chart.type) {
+        net.yy.google.chart.type = {};
+    }
+    if (!net.yy.google.chart.util) {
+        net.yy.google.chart.util = {};
+    }
 
-net.yy.google.chart.colors =
-    ['ff3333', 'ff8e8e', 'ff68dd', 'cc6600', 'ff9933', 'ffac6e',
-     'ffff00', 'fffe84', 'd1d17a', '336600', '1fcb4a', '66ff00',
-     '0066ff', '2f74d0', '2faace', '000066', '333399', '5b5bff',
-     '330066', 'f206ff', '800080'
-    ];
+    net.yy.google.chart.offline_test_mode = false;
+
+    if (typeof console === 'undefined' || !console) {
+        this.console = {};
+    }
+    if (!console.log) {
+        console.log = function () {};
+    }
+
+    net.yy.google.chart.colors =
+        ['ff3333', 'ff8e8e', 'ff68dd', 'cc6600', 'ff9933', 'ffac6e',
+         'ffff00', 'fffe84', 'd1d17a', '336600', '1fcb4a', '66ff00',
+         '0066ff', '2f74d0', '2faace', '000066', '333399', '5b5bff',
+         '330066', 'f206ff', '800080'
+        ];
+
+})();
